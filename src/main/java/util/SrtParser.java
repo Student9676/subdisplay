a5827407c9441d918ab017793876cfb945359553
Raasikh Kanjiani <64110149+Student9676@users.noreply.github.com>
2024-01-08 17:55:56 -0500

    Developed a basic version of the SrtParser.java file.

--- src/main/java/util/SrtParser.java
+++ src/main/java/util/SrtParser.java
@@ -1,6 +1,23 @@
 import util.Subtitle;
 import util.Movie;
+import java.util.ArrayList;
+import java.io.File;
 
 class SrtParser {
-    
+    public static Movie parse (String srtPath) {
+
+	Movie movie = null;
+	
+	File file = new File(srtPath);
+	Scanner scanner = new Scanner(file);
+	String srt = ""
+	while (scanner.hasNextLine()) {
+	    srt += scanner.nextLine();
+	}
+	scanner.close();
+
+	String sep = System.getProperty("line.separator");
+	ArrayList<String> rawSubtitles = new ArrayList<>(Arrays.asList(srt.split(sep + sep)));
+	
+    }
 }
