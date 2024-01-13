9a81cc02d6325134a9bc70f37a35e819368cf184
Raasikh Kanjiani <64110149+Student9676@users.noreply.github.com>
2024-01-13 02:43:50 -0500

    Changed internal time tracking to milliseconds

--- src/main/java/util/Subtitle.java
+++ src/main/java/util/Subtitle.java
@@ -3,20 +3,22 @@ package util;
 public class Subtitle {
 
     private int number;
-    private String startTime;
-    private String endTime;
+    private long startTime;
+    private long endTime;
     private String text;
-    private String duration;
+    private long duration;
     
     public Subtitle() {
 	this.number = 0;
-	this.startTime = this.endTime = this.text = this.duration = null;
+	this.startTime = this.endTime = this.duration = 0;
+	this.text = null;
     }
     
     public Subtitle(int number, String startTime, String endTime, String text) {
 	this.number = number;
-	this.startTime = startTime;
-	this.endTime = endTime;
+	this.startTime = this.parseTime(startTime);
+	this.endTime = this.parseTime(endTime);
+	this.duration = this.endTime - this.startTime;
 	this.text = text;
     }
 
@@ -28,27 +30,40 @@ public class Subtitle {
     public int getNumber() {
 	return this.number;
     }
-    public String getStartTime() {
+    public long getStartTime() {
 	return this.startTime;
     }
-    public String getEndTime() {
+    public long getEndTime() {
 	return this.endTime;
     }
     public String getText() {
-	return text;
+	return this.text;
+    }
+    public long getDuration() {
+	return this.duration;
     }
 
     public void setNumber(int num) {
 	this.number = num;
     }
     public void setStartTime(String time) {
-	this.startTime = time;
+	this.startTime = this.parseTime(time);
     }
     public void setEndTime(String time) {
-	this.endTime = time;
+	this.endTime = this.parseTime(time);
     }
     public void setText(String text) {
 	this.text = text;
+    }
+
+    private long parseTime(String timeStr) {
+	String[] timeVals = timeStr.split(":");
+	long hours = Long.parseLong(timeVals[0]);
+	long minutes = Long.parseLong(timeVals[1]);
+	long seconds = Long.parseLong(timeVals[2].split(",")[0]);
+	long millisecs = Long.parseLong(timeVals[2].split(",")[1]);
+
+	return (hours * 3600000) + (minutes * 60000) + (seconds * 1000) + millisecs; 
     }
 
 }
