db42bade6bdd67165b04718770584fc9a31216b1
Raasikh Kanjiani <64110149+Student9676@users.noreply.github.com>
2024-01-14 20:20:53 -0500

    Added a subtitle display time vs actual srt/video time delay logger to the prototype version

--- src/main/java/subdisplay/SubDisplay.java
+++ src/main/java/subdisplay/SubDisplay.java
@@ -9,61 +9,82 @@ import java.util.ArrayList;
 import util.SrtParser;
 import util.Movie;
 import util.Subtitle;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class SubDisplay {
 	public static void main(String[] args) {
 
-	Scanner scanner = new Scanner(System.in);
-	String srtPath;
-	while (true) {
-	    System.out.println("Provide a file path for your .srt file");
-	    System.out.flush();
-	    srtPath = scanner.nextLine();
-	    if (check_input(srtPath)) {
-		    break;
-	    }
-	}
-
-	Movie movie = SrtParser.parse(srtPath); 
-	ArrayList<String> strSubs = movie.getStringSubtitles();
-	ArrayList<Subtitle> parsedSubs = movie.getParsedSubtitles();
-	ArrayList<TimerTask> tasks = new ArrayList<>(); 
-	
-	System.out.println("Press Enter to start the subtitles");
-	System.out.flush();
-	scanner.nextLine();
-	scanner.close();
-	for (int k = 0; k < 4; k++) {
-	    deleteln();
-	}
-	
-	Timer timer = new Timer();
-	
-	for (int i = 0; i < strSubs.size(); i++) {
-	    final int index = i;
-	    tasks.add(new TimerTask() {
-		    @Override
-		    public void run() {
-			if (index > 0) {
-			    for (int j = 0; j < strSubs.get(index-1).lines().count(); j++) {
-				    deleteln();
-			    }
-                         } 
-			System.out.println(strSubs.get(index));  
-		    }
-		});
-	    timer.schedule(tasks.get(i), parsedSubs.get(i).getStartTime());
-	}
-
-	TimerTask cancelTask = new TimerTask() {
-		@Override
-		public void run() {
-		    timer.purge();
-		    timer.cancel();
+		Scanner scanner = new Scanner(System.in);
+		String srtPath;
+		while (true) {
+			System.out.println("Provide a file path for your .srt file");
+			System.out.flush();
+			srtPath = scanner.nextLine();
+			if (check_input(srtPath)) {
+			break;
+			}
 		}
-	};
-    timer.schedule(cancelTask, parsedSubs.get(parsedSubs.size() - 1).getEndTime() + 2000);
-    }
+
+		Movie movie = SrtParser.parse(srtPath); 
+		ArrayList<String> strSubs = movie.getStringSubtitles();
+		ArrayList<Subtitle> parsedSubs = movie.getParsedSubtitles();
+		ArrayList<TimerTask> tasks = new ArrayList<>(); 
+		
+		System.out.println("Press Enter to start the subtitles");
+		System.out.flush();
+		scanner.nextLine();
+		scanner.close();
+		for (int k = 0; k < 4; k++) {
+			deleteln();
+		}
+
+		try {
+			File delayLog = new File("delaylog.txt");
+			delayLog.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		long st = System.currentTimeMillis();
+
+		Timer timer = new Timer();
+		
+		for (int i = 0; i < strSubs.size(); i++) {
+			final int index = i;
+			tasks.add(new TimerTask() {
+			
+				@Override
+				public void run() {
+					if (index > 0) {
+						for (int j = 0; j < strSubs.get(index-1).lines().count(); j++) {
+							deleteln();
+						}
+					} 
+
+					System.out.println(strSubs.get(index));
+			
+					try {
+						FileWriter wr = new FileWriter("delayLog.txt", true);
+						wr.write(parsedSubs.get(index).getNumber() + " --> " + (System.currentTimeMillis() - st - parsedSubs.get(index).getStartTime()) + "\n");
+						wr.close();
+					} catch (IOException l) {
+						l.printStackTrace();
+					}
+				}
+			});
+			timer.schedule(tasks.get(i), parsedSubs.get(i).getStartTime());
+		}
+
+		TimerTask cancelTask = new TimerTask() {
+			@Override
+			public void run() {
+				timer.purge();
+				timer.cancel();
+			}
+		};
+		timer.schedule(cancelTask, parsedSubs.get(parsedSubs.size() - 1).getEndTime() + 2000);
+	}
 
     private static boolean check_input(String srtFilePath) {
 	try {
